26928fa8bb18d05091b8f96d2f0fd2285e1f1492
DeAndre Hoff <deandreryanhoff@yahoo.com>
2021-10-18 09:54:40 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('%c HI', 'color: firebrick')
+
+
+let breeds  =[]
+
+document.addEventListener('DOMContentLoaded', function(){
+    loadImages()
+    loadBreedOptions()
+    
+})
+
+const fun = (play) => {
+   return play.json()
+}
+function loadImages(){
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then( function play(response){
+    return response.json()
+    .then(results => {
+        results.message.forEach(image => addimage(image))
+    })
+} )
+
+}
+
+
